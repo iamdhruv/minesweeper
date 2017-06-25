6bb7c5f799b545db24a23951ca083582eae2eb6e
dp_gemini <dhruv.pahuja@geminisolutions.in>
2017-06-25 13:59:47 +0530

    removed ui ctrl

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
 var GameController = (function() {
-    var levels;
+    var levels, data;
 
-    var data = {
+    levels = {
+        1: {fieldSize: 4, noOfBombs: 1},
+        2: {fieldSize: 10, noOfBombs: 1},
+        3: {fieldSize: 15, noOfBombs: 1},
+        4: {fieldSize: 15, noOfBombs: 30},
+        5: {fieldSize: 20, noOfBombs: 30},
+        6: {fieldSize: 20, noOfBombs: 50},
+    }
+
+    data = {
         levelSelected: 0,
         bombsIndices: [],
         totalBlocks: 0,
         noOfBombs: 0,
         fieldSize: 0
     }
-    levels = {
-        1: {fieldSize: 10, noOfBombs: 10},
-        2: {fieldSize: 10, noOfBombs: 20},
-        3: {fieldSize: 20, noOfBombs: 20},
-        4: {fieldSize: 20, noOfBombs: 40},
-        5: {fieldSize: 30, noOfBombs: 60},
-        6: {fieldSize: 30, noOfBombs: 100},
+    
+    var compareNumbersForSort = function(n1, n2) {
+        return (n1 - n2); 
+    }
+
+    var getLevelMasterData = function(level) {
+        return levels[level];
     }
 
     var setLevelSelected = function(level) {
         data.levelSelected = level;
     }
 
-    var compareNumbersForSort = function(n1, n2) {
-        return (n1 - n2); 
-    }
-
     var setRandomBlockIndices = function(totalBlocks, noOfIndices) {
         var indices, randomNo;
         indices = [];
         for(var i=0; i<noOfIndices; i = i+1) {
-            randomNo = Math.floor(Math.random() * totalBlocks);
-            if(indices.indexOf(randomNo) === -1 && randomNo !==0) {
+            randomNo = Math.floor(Math.random() * (totalBlocks - 1));
+            if(indices.indexOf(randomNo) === -1) {
                 indices.push(randomNo);
             } else {
                 i = i-1;
@@ -41,7 +46,7 @@ var GameController = (function() {
 
     var getRandomBlocksHavingBombs = function() {
         var levelMasterData;
-        levelMasterData = levels[data.levelSelected];
+        levelMasterData = getLevelMasterData(data.levelSelected);
         data.totalBlocks = levelMasterData.fieldSize * levelMasterData.fieldSize;
         data.noOfBombs = levelMasterData.noOfBombs;
         data.fieldSize = levelMasterData.fieldSize;
@@ -52,16 +57,92 @@ var GameController = (function() {
     var getData = function() {
         return data;
     }
-    return {
-        setLevelSelected: setLevelSelected,
-        getRandomBlocksHavingBombs: getRandomBlocksHavingBombs,
-        getData: getData
-    }
-    
-})();
 
-var UIController = (function() {
-   
+    var getBlocksToCheck = function(currentBlockId) {
+        var i,j, blocksNeeded, blocksToCheck;
+        blocksNeeded = [];
+        i = Math.floor(currentBlockId / data.fieldSize);
+        j = (currentBlockId % data.fieldSize);
+        blocksToCheck = [
+            {row: (i-1), col: (j-1)},
+            {row: (i-1), col: (j)},
+            {row: (i-1), col: (j+1)},
+            {row: (i), col: (j-1)},
+            {row: (i), col: (j+1)},
+            {row: (i+1), col: (j-1)},
+            {row: (i+1), col: (j)},
+            {row: (i+1), col: (j+1)}
+        ];
+        blocksToCheck.forEach(function(item, index) {
+            if(item.row > -1 && item.col > -1 && item.row < (data.fieldSize) && item.col < (data.fieldSize)) {
+                blocksNeeded.push((data.fieldSize * item.row) + item.col);
+            }
+        });
+        return blocksNeeded;
+    }
+    var clickedBlockOperation = function(block) {
+        var response = adjacentBlocksOperations(block);
+        if(response) {
+            if(response.isBomb) {
+                block.style.backgroundColor = "red";
+                openAllBlocks();
+            }
+        }
+    }
+
+    var adjacentBlocksOperations = function(block) {
+        var currentBlockId, isBomb, isZero, isGreaterThanZero, blocksTocheck,res, isOpened,blocksNeeded, itemBlock;
+        blocksNeeded = [];
+        isOpened = block.classList.contains('opened');
+        if(isOpened) {
+            return;
+        }
+        currentBlockId = block.id;
+        isBomb = block.classList.contains('bomb');
+        adjacentBombs = block.getAttribute('data-no_of_adjacent_bombs');
+        isZero = adjacentBombs == 0 ? true : false;
+        isGreaterThanZero = adjacentBombs > 0 ? true : false;
+        if(isBomb) {
+            block.classList.add('opened');
+            return {
+                "isBomb": true
+            };
+        } else if(isZero) {
+            block.classList.add('opened');
+            blockId = currentBlockId.split('-')[1];
+            blocksTocheck = getBlocksToCheck(blockId);
+            console.log(currentBlockId);
+            console.log(blocksTocheck);
+            // blocksTocheck.forEach(function(item) {
+            //     if(item >= 1   && item <= (data.fieldSize * data.fieldSize)) {
+            //         res = adjacentBlocksOperations(document.getElementById('block-' + item));
+            //     }
+            // });
+        } else if(isGreaterThanZero) {
+            block.classList.add('opened');
+            block.innerHTML = block.getAttribute('data-no_of_adjacent_bombs');
+        }
+    }
+    var openAllBlocks = function() {
+        var isBomb, isZero, img;
+        img = document.createElement('img');
+        img.src = "img/bomb.svg";
+        var table = document.querySelector('.minesweeper-table').classList.add("table-outset-removed");
+        var blocks = document.querySelectorAll('.single-block');
+        for(var i=0; i<blocks.length; i++) {
+            blocks[i].classList.add("table-outset-removed");
+            isBomb = blocks[i].classList.contains('bomb');
+            isNotZero = blocks[i].getAttribute('data-no_of_adjacent_bombs') > 0 ? true : false;
+            if(isBomb) {
+                img = document.createElement('img');
+                img.src = "img/bomb.svg";
+                blocks[i].appendChild(img);
+            } else if(isNotZero) {
+                blocks[i].innerHTML = blocks[i].getAttribute('data-no_of_adjacent_bombs');
+            } 
+        }
+    }
+
     var addBlocks = function(fieldSize, blocksHavingBombs) {
 
         addBlankBlocks();
@@ -72,7 +153,7 @@ var UIController = (function() {
             for(var i=0; i<fieldSize; i++) {
                 partialString = '<tr>';
                 for(var j=0; j<fieldSize; j++) {
-                    partialString += '<td class="single-block" id="block-' + ((i * fieldSize) + (j+1)) +'"></td>';
+                    partialString += '<td class="single-block" id="block-' + ((i * fieldSize) + (j)) +'"></td>';
                 }
                 partialString += '</tr>';
                 document.querySelector('.minesweeper-field').insertAdjacentHTML('beforeend', partialString);
@@ -84,7 +165,7 @@ var UIController = (function() {
                     document.getElementById('block-' + element).classList.add('bomb');
                 });
                 addNumberInBlocks();
-
+                
                 function addNumberInBlocks() {
                     var blocksToCheckForBombs = [];
                     var currentBlock = 0; 
@@ -119,26 +200,60 @@ var UIController = (function() {
         }
     }
 
-    return {
-        addBlocks: addBlocks
+    var hideMineModal = function() {
+        document.getElementById('select-level').style.display = "none";
     }
 
+    return {
+        setLevelSelected: setLevelSelected,
+        getRandomBlocksHavingBombs: getRandomBlocksHavingBombs,
+        getData: getData,
+        clickedBlockOperation: clickedBlockOperation,
+        addBlocks: addBlocks,
+        hideMineModal: hideMineModal
+    }
+    
 })();
 
-var MainController = (function(GameCtrl, UICtrl) {
+var MainController = (function(GameCtrl) {
     var level, randomBlocksHavingBombs;
     
-    var init = function() {
-        level = prompt("Please enter level From 1 to 4", "");
+    function setSelectLevelBtnListener() {
+        var allLevelButtons;
+        allLevelButtons = document.querySelectorAll('.level-select-btn');
+        for(var i=0; i<allLevelButtons.length; i++) {
+            allLevelButtons[i].addEventListener('click', startGame);
+        }
+    }
+    
+    function startGame() {
+        var buttonId, level;
+        GameCtrl.hideMineModal();
+        buttonId = this.id;
+        level = buttonId.split('-')[2];
         GameCtrl.setLevelSelected(level);
         randomBlocksHavingBombs = GameCtrl.getRandomBlocksHavingBombs(level);
         data = GameCtrl.getData();
-        UICtrl.addBlocks(data.fieldSize, randomBlocksHavingBombs);
+        GameCtrl.addBlocks(data.fieldSize, randomBlocksHavingBombs);
+    }
+
+    function blockClickListener(event) {
+        if(event.target && event.target.classList.contains('single-block')) {
+            GameCtrl.clickedBlockOperation(event.target);
+        }
+    }
+    function setUpEventListeners() {
+        setSelectLevelBtnListener();
+        document.querySelector('.minesweeper-table').addEventListener("click", blockClickListener);
+    }
+
+    var init = function() {
+        setUpEventListeners();
     };
     
     return {
         init: init
     }
-}(GameController, UIController));
+}(GameController));
 
 MainController.init();
